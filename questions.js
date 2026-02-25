c3736e19fc31708d862daf6f1c9537548c7ed93f
Aysu Maharramova <125694685+aysumaharramovaa@users.noreply.github.com>
2026-02-25 10:36:19 +0400

    first commit

---
+++ questions.js
@@ -0,0 +1,79 @@
+const questions = [
+
+{
+question:"HTML nədir?",
+correct:"Markup language",
+wrong:["Programming language","Database","Operating System","Compiler"]
+},
+{
+question:"CSS nə üçündür?",
+correct:"Design üçün",
+wrong:["Database üçün","Server üçün","AI üçün","Game engine"]
+},
+{
+question:"JS nə üçündür?",
+correct:"Səhifəni interaktiv etmək",
+wrong:["Yalnız dizayn","Yalnız database","OS qurmaq","Kompilyasiya"]
+},
+{
+question:"2 + 2 = ?",
+correct:"4",
+wrong:["5","3","6","8"]
+},
+{
+question:"Azərbaycanın paytaxtı?",
+correct:"Bakı",
+wrong:["Gəncə","Sumqayıt","Şəki","Lənkəran"]
+},
+{
+question:"React nədir?",
+correct:"JS library",
+wrong:["Database","Server","Browser","OS"]
+},
+{
+question:"Python nədir?",
+correct:"Programming language",
+wrong:["Car brand","Phone","Game","Browser"]
+},
+{
+question:"1 byte neçə bitdir?",
+correct:"8",
+wrong:["4","16","2","32"]
+},
+{
+question:"Internet nədir?",
+correct:"Şəbəkə sistemi",
+wrong:["Telefon","Printer","Monitor","Keyboard"]
+},
+{
+question:"CPU nədir?",
+correct:"Processor",
+wrong:["RAM","Hard disk","Mouse","Speaker"]
+},
+{
+question:"RAM nə üçündür?",
+correct:"Müvəqqəti yaddaş",
+wrong:["Əbədi yaddaş","Printer","CPU","Monitor"]
+},
+{
+question:"JS harada işləyir?",
+correct:"Browserdə",
+wrong:["Yalnız printerdə","Yalnız BIOS-da","Mouse-da","USB-də"]
+},
+{
+question:"CSS açılımı?",
+correct:"Cascading Style Sheets",
+wrong:["Computer Style System","Creative Sheet System","Color Style Syntax","Coding Style Script"]
+},
+{
+question:"HTML açılımı?",
+correct:"Hyper Text Markup Language",
+wrong:["High Text Machine Language","Home Tool Markup Language","Hyper Tool Multi Language","None"]
+},
+{
+question:"Which one is database?",
+correct:"MySQL",
+wrong:["HTML","CSS","Chrome","Photoshop"]
+}
+
+];
